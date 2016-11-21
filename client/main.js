ed57e3c7723576df688bc251ed80fe4394efd7e0
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-11-21 18:00:46 +0000

    Getting actual article data

--- client/main.js
+++ client/main.js
@@ -1,7 +1,9 @@
 import { bootstrap } from 'n-ui';
 import nUiConfig from './n-ui-config';
+import { lazyLoad as lazyLoadImages } from 'n-image';
 import OVideo from 'o-video';
 
 bootstrap(nUiConfig, () => {
 	OVideo.init();
+	lazyLoadImages();
 });
--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,5 +1,32 @@
 const query = id => {
-	return `{
+	return `
+	fragment sliceContent on Tag {
+		prefLabel
+		relativeUrl
+		contentCount
+		latestContent(limit: 4, type: Video) {
+			...teaserContent
+		}
+	}
+	fragment teaserContent on Content {
+		id
+		title
+		lastPublished
+		tag: teaserTag {
+			url
+			name
+		}
+		genreTag {
+			prefLabel
+		}
+		image: mainImage {
+			src: rawSrc
+			width
+			height
+			ratio
+		}
+	}
+	{
 		video {
 			${
 				id ?
@@ -17,33 +44,21 @@ const query = id => {
 				title
 				duration
 				image {
-					rawSrc
+					src: rawSrc
 				}
 			}
-			world: section(id: "MQ==-U2VjdGlvbnM=", limit: 4) {
-				id
-				title
-				duration
-				image {
-					rawSrc
-				}
-			}
-			lex: section(id: "MTE1-U2VjdGlvbnM=", limit: 4) {
-				id
-				title
-				duration
-				image {
-					rawSrc
-				}
-			}
-			companies: section(id: "Mjk=-U2VjdGlvbnM=", limit: 4) {
-				id
-				title
-				duration
-				image {
-					rawSrc
-				}
-			}
+		}
+		markets: tag(id: "NzE=-U2VjdGlvbnM=") {
+			...sliceContent
+		}
+		world: tag(id: "MQ==-U2VjdGlvbnM=") {
+			...sliceContent
+		}
+		companies: tag(id: "Mjk=-U2VjdGlvbnM=") {
+			...sliceContent
+		}
+		lifeAndArts: tag(id: "MTQ4-U2VjdGlvbnM=") {
+			...sliceContent
 		}
 	}`;
 };
@@ -56,31 +71,26 @@ module.exports = (req, res, next) => {
 		headers: {}
 	})
 		.then(response => response.json())
-		.then(({ data: { video: videos = {} } = {} } = {}) => {
+		.then(({ data: { video: videos = {}, markets, companies, world, lifeAndArts } = {} } = {}) => {
 			const video = Array.isArray(videos.video) ? videos.video[0] : videos.video;
-			const sections = [
+			const sliceData = [markets, companies, world, lifeAndArts].map(tag => ({
+				title: tag.prefLabel,
+				url: tag.relativeUrl,
+				videoCount: tag.contentCount,
+				videos: tag.latestContent
+			}));
+			const slices = [
 				{
 					title: 'Editor‘s Picks',
 					videos: videos.editorsPicks
 				},
-				{
-					title: 'World',
-					videos: videos.world
-				},
-				{
-					title: 'Lex',
-					videos: videos.lex
-				},
-				{
-					title: 'Companies',
-					videos: videos.companies
-				}
+				...sliceData
 			];
 			res.render('video', {
 				layout: 'wrapper',
 				title: `Financial Times | Video | ${video.title}`,
 				video,
-				sections
+				slices
 			});
 		})
 		.catch(next);
