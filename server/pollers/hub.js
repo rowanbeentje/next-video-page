b8b0ad2dff06e1d2e64cb8b3d7b0ad080d350af6
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-11-24 17:36:36 +0000

    Add poller for hub data

---
+++ server/pollers/hub.js
@@ -0,0 +1,58 @@
+const logger = require('@financial-times/n-logger');
+const createError = require('http-errors');
+
+const hubQuery = require('../queries/hub');
+
+class HubPoller {
+
+	constructor ({ refreshInterval = 60 } = {}) {
+		this.refreshInterval = refreshInterval * 1000;
+		this.data = null;
+		this.currentFetch = null;
+	}
+
+	start () {
+		this.update();
+		setInterval(this.update, this.refreshInterval);
+	}
+
+	update () {
+		if (this.currentFetch) {
+			return this.currentFetch;
+		}
+		return this.currentFetch = fetch(`https://next-api.ft.com/v1/query?query=${encodeURIComponent(hubQuery)}&source=next-video-page`, {
+			timeout: 3000,
+			headers: {
+				'X-Api-Key': process.env.NEXT_API_KEY
+			}
+		})
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					return response.text()
+						.then(text => {
+							throw createError(response.status, text);
+						});
+				}
+			})
+			.catch(logger.error)
+			.then(data => {
+				this.currentFetch = null;
+				if (data) {
+					this.data = data
+				}
+				return this.data;
+			});
+	}
+
+	getData () {
+		// if we have data, use that, otherwise wait for the current request to return
+		return this.data ? Promise.resolve(this.data) : this.update();
+	}
+
+}
+
+const hubPoller = new HubPoller();
+
+module.exports = hubPoller;
