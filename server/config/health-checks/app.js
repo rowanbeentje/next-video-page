95cc9cc66233616ee4bb4a7cfcab3f354f3e5ff7
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-11-28 17:37:43 +0000

    fix apostrophe linting

--- server/config/health-checks/app.js
+++ server/config/health-checks/app.js
@@ -9,7 +9,7 @@ module.exports = {
 			technicalSummary: 'Check there haven‘t been more than 10 5xx responses from the app in the last 10 minutes',
 			panicGuide: 'Check Grafana (http://grafana.ft.com/dashboard/db/next-video-page?panelId=5&fullscreen) for `5xx` related errors and Splunk (http://bit.ly/2gbqIWh) and Sentry (https://sentry.io/nextftcom/ft-next-video-page/) for error logging',
 			type: 'graphiteThreshold',
-			metric: `integral(heroku.api.web_*.express.query_*.res.status.5*.count)`,
+			metric: 'integral(heroku.api.web_*.express.query_*.res.status.5*.count)',
 			samplePeriod: '10min',
 			threshold: 10
 		},
@@ -20,7 +20,7 @@ module.exports = {
 			technicalSummary: 'Check there haven‘t been any 5xx responses from the app in the last 10 minutes',
 			panicGuide: 'Check Grafana (http://grafana.ft.com/dashboard/db/next-video-page?panelId=5&fullscreen) for `5xx` related errors and Splunk (http://bit.ly/2gbqIWh) and Sentry (https://sentry.io/nextftcom/ft-next-video-page/) for error logging',
 			type: 'graphiteThreshold',
-			metric: `heroku.api.web_*.express.query_*.res.status.5*.count`,
+			metric: 'integral(heroku.api.web_*.express.query_*.res.status.5*.count)',
 			samplePeriod: '10min',
 			threshold: 0
 		}
